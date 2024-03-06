d1c593e4caaf233223e8caa78425db4b8a393379
DeHkO1988 <denislav.bogomilov88@gmail.com>
2024-03-06 23:23:45 +0200

    home register login add - html, css, routing

--- RecipeShareSite/src/app/app.module.ts
+++ RecipeShareSite/src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
