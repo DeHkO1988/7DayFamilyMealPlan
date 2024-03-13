c23458239f92501a27f9978873a2389e6f1b70e1
DeHkO1988 <denislav.bogomilov88@gmail.com>
2024-03-13 22:48:44 +0200

    on edit page populate data in the form

--- RecipeShareSite/src/app/edit-recipe/edit-recipe.component.ts
+++ RecipeShareSite/src/app/edit-recipe/edit-recipe.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TokenApiService } from '../token-api.service';
+import { Recipe } from '../types/recipe';
 
 @Component({
   selector: 'app-edit-recipe',
   templateUrl: './edit-recipe.component.html',
   styleUrls: ['./edit-recipe.component.css']
 })
-export class EditRecipeComponent {
+export class EditRecipeComponent implements OnInit {
+
+  currentRecipeInfo: Recipe | undefined = undefined;
+
+  constructor(private route: ActivatedRoute, private tokenService: TokenApiService) { }
+
+  ngOnInit(): void {
+
+    const id = this.route.snapshot.params["id"];
+
+    this.tokenService.getOne(id).subscribe(data => {
+      this.currentRecipeInfo = data;
+    })
+
+
+  }
 
 }
